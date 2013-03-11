f7a2ea466f1c37a4b2d576fe5a29579b2eb2d2a1
Mike Castle <dalgoda@gmail.com>
2013-03-10 23:19:27 -0700

    New time function.

--- py/humanize.py
+++ py/humanize.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 """Provide human readable output."""
 
+import datetime
+
 def binary_size(num):
   """Return size with a suitable 1024 based."""
   for pre in ['bytes', 'KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB']:
@@ -8,3 +10,7 @@ def binary_size(num):
       return "%3.1f%s" % (num, pre)
     num /= 1024.0
   return 'BIG'
+
+def time(seconds):
+  """Return hh:mm:ss when given seconds."""
+  return str(datetime.timedelta(seconds=seconds))
