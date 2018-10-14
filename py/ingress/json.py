51d3b301890054bd1928d2bd888ed847aa4be25a
Mike Castle <dalgoda@gmail.com>
2018-10-13 22:53:25 -0700

    Mostly make imports work because "import json" was importing itself. Also, yapf and pylint.

--- py/ingress/json.py
+++ py/ingress/json.py
@@ -1,22 +1,30 @@
 """Utilities to work with JSON files."""
 
+from __future__ import absolute_import
+
 import codecs
 import json
+import os
+import tempfile
+import time
+
 
 def load(json_name):
-  data = json.load(codecs.open(json_name, encoding='utf-8'))
-  return data
+    """Load a utf8-encoded json file."""
+    data = json.load(codecs.open(json_name, encoding='utf-8'))
+    return data
+
 
 def save(db_name, data):
-  newname = '%s.%s' % (db_name, time.strftime('%Y-%m-%dT%H:%M'))
-  tmp_fd, tmp_filename = tempfile.mkstemp(prefix='ingress_data', dir='.')
-  os.close(tmp_fd)
-  #tmp_handle = os.fdopen(tmp_fd, 'w')
-  tmp_handle = codecs.open(tmp_filename, 'w', encoding='utf-8')
-  json.dump(data, tmp_handle, indent=2, sort_keys=True)
-  tmp_handle.close()
-  try:
-    os.rename(db_name, newname)
-  except OSError, e:
-    pass
-  os.rename(tmp_filename, db_name)
+    """Atomically save a utf8-encoded json file."""
+    newname = '%s.%s' % (db_name, time.strftime('%Y-%m-%dT%H:%M'))
+    tmp_fd, tmp_filename = tempfile.mkstemp(prefix='ingress_data', dir='.')
+    os.close(tmp_fd)
+    tmp_handle = codecs.open(tmp_filename, 'w', encoding='utf-8')
+    json.dump(data, tmp_handle, indent=2, sort_keys=True)
+    tmp_handle.close()
+    try:
+        os.rename(db_name, newname)
+    except OSError:
+        pass
+    os.rename(tmp_filename, db_name)
