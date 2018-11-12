bb4ea0ead1a02a9c584f615f221e8b96992aa5f6
Mike Castle <dalgoda@gmail.com>
2018-11-11 21:46:55 -0800

    Now that we want to do something with the optimized list, we need the data back.  So package it up into a list of objects (portals and legs).

--- py/ingress/routes.py
+++ py/ingress/routes.py
@@ -1,6 +1,8 @@
 """Create and save routes between portals."""
 
+import collections
 import os
+import toposort
 
 from ingress import bookmarks
 from ingress import database
@@ -13,23 +15,25 @@ def route(args, dbc):
     portals = bookmarks.load(args.bookmarks)
     portal_keys = portals.keys()
     portal_keys.append(portal_keys[0])
-    optimized_path = tsp.optimize(
+    optimized_info = tsp.optimize(
         portal_keys,
         lambda start, end: _cost(dbc, mode_cost_map, args.walk_auto, start, end)
     )
 
     basename = os.path.splitext(args.bookmarks)[0]
 
-    _save_as_kml(basename, optimized_path)
-    _save_as_bookmarks(basename, optimized_path)
-    _save_as_text(basename, optimized_path)
-    _save_as_drawtools(basename, optimized_path)
+    path_info = _path_info(dbc, optimized_info[1], mode_cost_map)
+    print path_info
+    _save_as_kml(basename, path_info)
+    _save_as_bookmarks(basename, path_info)
+    _save_as_text(basename, path_info)
+    _save_as_drawtools(basename, path_info)
 
 
 def _cost(dbc, mode_cost_map, max_walking_time_allowed, begin, end):
-    cost = mode_cost_map.get((begin, end))
-    if cost:
-        return cost
+    mode_cost = mode_cost_map.get((begin, end))
+    if mode_cost:
+        return mode_cost[1]
     costs = dict()
     begin_portal = dbc.session.query(database.Portal).get(begin)
     end_portal = dbc.session.query(database.Portal).get(end)
@@ -44,9 +48,11 @@ def _cost(dbc, mode_cost_map, max_walking_time_allowed, begin, end):
     walking_to_driving = 1.0 * walking / driving
     if walking < max_walking_time_allowed or walking_to_driving < 1.1:
         cost = walking
+        mode = 'walking'
     else:
         cost = driving
-    mode_cost_map[(begin, end)] = cost
+        mode = 'driving'
+    mode_cost_map[(begin, end)] = (mode, cost)
     return cost
 
 
@@ -58,6 +64,31 @@ def _path_cost(dbc, path):
         cost += leg.duration
 
     return cost
+
+
+def _path_info(dbc, opt_path, mode_cost_map):
+    items = list()
+    for begin, end in zip(opt_path, opt_path[1:]):
+        mode, cost = mode_cost_map[(begin, end)]
+        db_begin = dbc.session.query(database.Portal).get(begin)
+        db_end = dbc.session.query(database.Portal).get(end)
+        items.append(('portal', db_begin))
+        db_path = dbc.session.query(database.Path).filter(
+            database.Path.mode == mode,
+            database.Path.begin_latlng == db_begin.latlng,
+            database.Path.end_latlng == db_end.latlng).one()
+        legs_set = collections.defaultdict(set)
+        legs_dict = dict()
+        for db_leg in dbc.session.query(database.Leg).join(
+                database.PathLeg).filter(
+                    database.PathLeg.path_id == db_path.id):
+            legs_set[db_leg.begin_latlng].add(db_leg.end_latlng)
+            legs_dict[db_leg.begin_latlng] = db_leg
+        sorted_legs = list(reversed(toposort.toposort_flatten(legs_set)))
+        items.append(
+            ('legs', [legs_dict[latlng] for latlng in sorted_legs[:-1]]))
+
+    return items
 
 
 def _save_as_kml(basename, path):
