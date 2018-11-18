ac06863fb366abbe55c91c5a9fdc733a03049462
Mike Castle <dalgoda@gmail.com>
2018-11-17 20:42:01 -0800

    Implement codes-set command.
    Combined (create, update) and renamed set.
    Renamed read to get.
    
    Implemented an argument parser for common code flags.
    
    Renamed functions to nouns for consistency, The set command cannot use
    set() because it is a reserved word, so used setter() instead.

--- py/ingress/codes.py
+++ py/ingress/codes.py
@@ -1,49 +1,87 @@
 """Functions to manage and use location codes associated with portals."""
 
+from ingress import database
+
+_BOOLY = {
+    'true': True,
+    'True': True,
+    'false': False,
+    'False': False,
+}
+
 
 def register_module_parsers(ctx):
     """Parser registration API."""
+    code_parser = ctx.argparse.ArgumentParser(add_help=False)
+    code_parser.add_argument(
+        '-c',
+        '--code',
+        action='store',
+        type=unicode,
+        required=True,
+        help='The location code.')
+    code_parser.add_argument(
+        '-l',
+        '--label',
+        action='store',
+        type=unicode,
+        help='Label for the location code.')
+    code_parser.add_argument(
+        '-k',
+        '--keep',
+        type=lambda x: _BOOLY.get(x, x),
+        choices=(True, False),
+        help=('Controls whether or not to keep portals with this location code'
+              ' during the prune operations.'))
     parser = ctx.subparsers.add_parser(
-        'codes-create', description=create.__doc__, help=create.__doc__)
-    parser.set_defaults(func=create)
+        'codes-set',
+        parents=[code_parser],
+        description=setter.__doc__,
+        help=setter.__doc__)
+    parser.set_defaults(func=setter)
 
     parser = ctx.subparsers.add_parser(
-        'codes-read', description=read.__doc__, help=read.__doc__)
-    parser.set_defaults(func=read)
+        'codes-get',
+        parents=[code_parser],
+        description=getter.__doc__,
+        help=getter.__doc__)
+    parser.set_defaults(func=getter)
 
     parser = ctx.subparsers.add_parser(
-        'codes-update', description=update.__doc__, help=update.__doc__)
-    parser.set_defaults(func=update)
+        'codes-delete',
+        parents=[code_parser],
+        description=deleter.__doc__,
+        help=deleter.__doc__)
+    parser.set_defaults(func=deleter)
 
     parser = ctx.subparsers.add_parser(
-        'codes-delete', description=delete.__doc__, help=delete.__doc__)
-    parser.set_defaults(func=read)
-
-    parser = ctx.subparsers.add_parser(
-        'codes-prune', description=prune.__doc__, help=prune.__doc__)
-    parser.set_defaults(func=read)
+        'codes-prune', description=pruner.__doc__, help=pruner.__doc__)
+    parser.set_defaults(func=pruner)
 
 
-def create(args, dbc):
-    """Create a new location code."""
+def setter(args, dbc):
+    """Sets a location code, creating it if necessary."""
+    db_code = dbc.session.query(database.Code).get(args.code)
+    if db_code is None:
+        db_code = database.Code(code=args.code)
+    if args.keep is not None:
+        db_code.keep = args.keep
+    if args.label is not None:
+        db_code.label = args.label
+    dbc.session.add(db_code)
+    dbc.session.commit()
+
+
+def getter(args, dbc):
+    """Display one or more location codes."""
     pass
 
 
-def read(args, dbc):
-    """Display location codes."""
-    pass
-
-
-def update(args, dbc):
-    """Update an existing location code."""
-    pass
-
-
-def delete(args, dbc):
+def deleter(args, dbc):
     """Delete a location code."""
     pass
 
 
-def prune(args, dbc):
+def pruner(args, dbc):
     """Prune portals based upon keep status of location codes."""
     pass
