165c5f636ff497d5fb90b7848f10410c5cf5fe1a
Mike Castle <dalgoda@gmail.com>
2024-11-28 12:23:03 -0800

    Migrate `address-*` commands to subcommands.
    Issue #31.

--- ingress/addresses.py
+++ ingress/addresses.py
@@ -20,31 +20,40 @@ if typing.TYPE_CHECKING:  # pragma: no cover
 MAX_AGE = 90 * 24 * 60 * 60
 
 
+class Error(Exception):
+    """Base module exception."""
+
+
 def mundane_commands(ctx: app.ArgparseApp):
     """Parser registration API."""
     bm_flags = ctx.get_shared_parser('bookmarks')
 
-    address_type_flag = ctx.argparse_api.ArgumentParser(add_help=False)
-    address_type_flag.add_argument(
+    type_flag = ctx.argparse_api.ArgumentParser(add_help=False)
+    type_flag.add_argument(
         '-t',
         '--type',
         action='store',
         required=True,
         help='Address type to modify.')
 
-    address_value_flag = ctx.argparse_api.ArgumentParser(add_help=False)
-    address_value_flag.add_argument(
+    value_flag = ctx.argparse_api.ArgumentParser(add_help=False)
+    value_flag.add_argument(
         '-v',
         '--value',
         action='store',
         required=True,
-        help='Value portion of address/value to modify.')
+        help='Value portion of (address, value) to modify.')
 
     note_flag = ctx.argparse_api.ArgumentParser(add_help=False)
     note_flag.add_argument(
         '-N', '--note', action='store', help='Optional note to add')
 
-    parser = ctx.register_command(address_update, parents=[bm_flags])
+    parser = ctx.register_command(address)
+    parser.set_defaults(func=lambda x, y=parser: _usage(x, y))
+    address_cmds = ctx.new_subparser(parser)
+
+    parser = ctx.register_command(
+        update, subparser=address_cmds, parents=[bm_flags])
     parser.add_argument(
         '-l',
         '--limit',
@@ -64,10 +73,24 @@ def mundane_commands(ctx: app.ArgparseApp):
             'Average random delay, in seconds, between each fetch.'
             '  (Default: %(default)s)'))
 
-    ctx.register_command(address_type_list)
+    parser = ctx.register_command(prune, subparser=address_cmds)
+    parser.add_argument(
+        '--commit',
+        default=False,
+        action=ctx.argparse_api.BooleanOptionalAction,
+        help='Commit the pruning operation. (Default: %(default)s)')
+
+    parser = ctx.register_command(type_, name='type', subparser=address_cmds)
+    parser.set_defaults(func=lambda x, y=parser: _usage(x, y))
+    type_cmds = ctx.new_subparser(parser)
+
+    ctx.register_command(type_list, name='list', subparser=type_cmds)
 
     parser = ctx.register_command(
-        address_type_set, parents=[address_type_flag, note_flag])
+        type_set,
+        name='set',
+        subparser=type_cmds,
+        parents=[type_flag, note_flag])
     parser.add_argument(
         '-v',
         '--visibility',
@@ -75,13 +98,20 @@ def mundane_commands(ctx: app.ArgparseApp):
         choices=('show', 'hide'),
         help='The visibility of given address type.')
 
-    ctx.register_command(address_type_delete, parents=[address_type_flag])
+    ctx.register_command(
+        type_del, name='del', subparser=type_cmds, parents=[type_flag])
 
-    ctx.register_command(address_type_value_list)
+    parser = ctx.register_command(value, subparser=type_cmds)
+    parser.set_defaults(func=lambda x, y=parser: _usage(x, y))
+    value_cmds = ctx.new_subparser(parser)
+
+    ctx.register_command(value_list, name='list', subparser=value_cmds)
 
     parser = ctx.register_command(
-        address_type_value_set,
-        parents=[address_type_flag, address_value_flag, note_flag])
+        value_set,
+        name='set',
+        subparser=value_cmds,
+        parents=[type_flag, value_flag, note_flag])
     parser.add_argument(
         '-p',
         '--pruning',
@@ -89,22 +119,41 @@ def mundane_commands(ctx: app.ArgparseApp):
         choices=('remove', 'ignore'),
         help=(
             'Determines how the pruning operation should treat portals'
-            ' with this address type/value.'))
+            ' with this address (type, value).'))
 
     ctx.register_command(
-        address_type_value_delete,
-        parents=[address_type_flag, address_value_flag])
+        value_del,
+        name='del',
+        subparser=value_cmds,
+        parents=[type_flag, value_flag])
 
-    parser = ctx.register_command(address_prune)
-    parser.add_argument(
-        '--commit',
-        default=False,
-        action=ctx.argparse_api.BooleanOptionalAction,
-        help='Commit the pruning operation. (Default: %(default)s)')
+
+def _usage(args: argparse.Namespace, parser: argparse.ArgumentParser) -> int:
+    """A function called simply to display help output."""
+    del args
+
+    parser.print_help()
+
+    return 1
+
+
+def address(args: argparse.Namespace) -> int:
+    """(V) A family of address commands."""
+    raise Error('This function should never be called.')
+
+
+def type_(args: argparse.Namespace) -> int:
+    """(V) A family of (address, type) commands."""
+    raise Error('This function should never be called.')
+
+
+def value(args: argparse.Namespace) -> int:
+    """(V) A family of (address, type, value) commands."""
+    raise Error('This function should never be called.')
 
 
 # pylint: disable=duplicate-code
-def address_update(args: argparse.Namespace) -> int:
+def update(args: argparse.Namespace) -> int:
     """(V) Update address related data for portals in a BOOKMARKS file.
 
     This command uses the Google Maps API to fetch address and other data
@@ -117,7 +166,7 @@ def address_update(args: argparse.Namespace) -> int:
     Hint: A LIMIT may be set to prevent the command from going through your
     quota.
 
-    Hint: See 'address-type-*' and 'address-type-value-*' family of commands
+    Hint: See 'address type' and 'address type value' families of commands
     to examine and control how some of this data may be used to affect other
     commands.
 
@@ -134,8 +183,7 @@ def address_update(args: argparse.Namespace) -> int:
     delay = 0.0
     for portal in portals.values():
         latlng = portal['latlng']
-        address = dbc.session.get(database.Address, latlng)
-        if address is None:
+        if dbc.session.get(database.Address, latlng) is None:
             if fetched:
                 delay = _random_delay(delay_base)
             print(
@@ -156,12 +204,12 @@ def address_update(args: argparse.Namespace) -> int:
     return 0
 
 
-def address_type_list(args: argparse.Namespace) -> int:
+def type_list(args: argparse.Namespace) -> int:
     """(V) List the known address types and current settings.
 
-    These values are populated by the 'address-update' command.
+    These values are populated by the 'address update' command.
 
-    Hint: Use the 'address-type-set' and 'address-type-delete' commands to
+    Hint: Use the 'address type set' and 'address type delete' commands to
     modify the settings.
     """
     dbc = args.dbc
@@ -181,14 +229,14 @@ def address_type_list(args: argparse.Namespace) -> int:
     return 0
 
 
-def address_type_set(args: argparse.Namespace) -> int:
+def type_set(args: argparse.Namespace) -> int:
     """(V) Update settings on a known address type.
 
     Setting the visibility to "hide" will hide it in the
-    'address-type-value-list' output and prevent the type from affecting
+    'address type value list' output and prevent that type from affecting
     dependent operations.
 
-    These values are populated by the 'address-update' command.
+    These values are populated by the 'address update' command.
     """
     dbc = args.dbc
     address_type = dbc.session.get(database.AddressType, args.type)
@@ -206,16 +254,16 @@ def address_type_set(args: argparse.Namespace) -> int:
     return ret
 
 
-def address_type_delete(args: argparse.Namespace) -> int:
+def type_del(args: argparse.Namespace) -> int:
     """(V) Delete a known address type.
 
-    Deleting an address type will also delete the related (address, type,
-    value) combinations.
+    Deleting an address type will also delete the related address
+    (type, value) combinations.
 
     Mostly this is used to reduce clutter while fine-tuning settings during
     initial configuration or moving to a new area.
 
-    These values are populated by the 'address-update' command.
+    These values are populated by the 'address update' command.
     """
     dbc = args.dbc
     address_type = dbc.session.get(database.AddressType, args.type)
@@ -229,12 +277,12 @@ def address_type_delete(args: argparse.Namespace) -> int:
     return ret
 
 
-def address_type_value_list(args: argparse.Namespace) -> int:
+def value_list(args: argparse.Namespace) -> int:
     """(V) List the known address (types, values) and settings.
 
-    These values are populated by the 'address-update' command.
+    These values are populated by the 'address update' command.
 
-    Hint: Use the 'address-type-value-set' and 'address-type-value-delete'
+    Hint: Use the 'address type value set' and 'address type value del'
     commands to modify the settings.
     """
     dbc = args.dbc
@@ -264,12 +312,12 @@ def address_type_value_list(args: argparse.Namespace) -> int:
     return 0
 
 
-def address_type_value_set(args: argparse.Namespace) -> int:
+def value_set(args: argparse.Namespace) -> int:
     """(V) Update settings on a known address (type, value).
 
     These settings are used to control other commands.
 
-    These values are populated by the 'address-update' command.
+    These values are populated by the 'address update' command.
     """
     dbc = args.dbc
     address_type_value = dbc.session.get(
@@ -283,17 +331,17 @@ def address_type_value_set(args: argparse.Namespace) -> int:
         dbc.session.add(address_type_value)
         dbc.session.commit()
     else:
-        print(f'Unknown address type/value: "{args.type}"/"{args.value}"')
+        print(f'Unknown address (type, value): "{args.type}"/"{args.value}"')
         ret = 1
     return ret
 
 
-def address_type_value_delete(args: argparse.Namespace) -> int:
+def value_del(args: argparse.Namespace) -> int:
     """(V) Delete a known address (type, value).
 
     Mostly this is used for testing.
 
-    These values are populated by the 'address-update' command.
+    These values are populated by the 'address update' command.
     """
     dbc = args.dbc
     address_type_value = dbc.session.get(
@@ -308,14 +356,14 @@ def address_type_value_delete(args: argparse.Namespace) -> int:
     return ret
 
 
-def address_prune(args: argparse.Namespace) -> int:
+def prune(args: argparse.Namespace) -> int:
     """(V) Remove portals from the database that do not match criteria.
 
     Prune portals from the database where its address has an
-    address-type-value for the pruning operation set to "remove".
+    (type, value) for the pruning operation set to "remove".
 
-    Hint: See the 'address-update', 'address-type-*', and
-    'address-type-value-*' family of command for more information.
+    Hint: See the 'address update', 'address type', and
+    'address type value' families of commands for more information.
     """
     dbc = args.dbc
 
