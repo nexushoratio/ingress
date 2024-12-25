f390064ef47771841a0e37866339febef6225254
Mike Castle <dalgoda@gmail.com>
2024-12-24 17:38:26 -0800

    Tweak `PRAGMA optimize` log.
    Issue #41.

--- ingress/database.py
+++ ingress/database.py
@@ -631,7 +631,7 @@ class Database:  # pylint: disable=missing-class-docstring
         """Maintenance on close."""
         conn = kwargs['dbapi_connection']
         for row in conn.execute('PRAGMA optimize(-1)'):
-            logging.info('optimize: %s', row)
+            logging.info('optimize: %s', row[0])
         count = conn.execute('PRAGMA freelist_count').fetchone()[0]
         logging.info('freelist_count: %d', count)
         self._frag_check(conn)
