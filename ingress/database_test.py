4178722ac9796275a4b6f277b1979fcf5cae111b
Mike Castle <dalgoda@gmail.com>
2025-01-05 11:32:59 -0800

    Test the `portals` to `v2_portals` table migration.
    Issue #41.

--- ingress/database_test.py
+++ ingress/database_test.py
@@ -4,6 +4,7 @@
 
 import argparse
 import pathlib
+import sqlite3
 import unittest
 
 from mundane import app
@@ -81,6 +82,54 @@ class DatabaseTest(unittest.TestCase):
         self.assertFalse(db_path.exists())
         self.assertIsInstance(dbc.session, database.sqlalchemy.orm.Session)
         self.assertTrue(db_path.exists())
+
+    def test_portals_v2_migration(self):
+        dbc = test_helper.database_connection(self)
+        db_path = pathlib.Path(dbc._directory, dbc._filename)
+        conn = sqlite3.connect(db_path)
+        dbc._connect(dbapi_connection=conn)
+        # Darn, unable to use the 'CreateTable' output here because GEOMETRY.
+        ddl = """
+CREATE TABLE portals (
+    guid VARCHAR NOT NULL,
+    label VARCHAR NOT NULL,
+    first_seen INTEGER NOT NULL,
+    last_seen INTEGER NOT NULL,
+    latlng GEOMETRY,
+    CONSTRAINT pk_portals PRIMARY KEY (guid)
+)
+"""
+        conn.execute(ddl)
+        conn.execute(
+            'SELECT RecoverGeometryColumn(?,?,?,?,?)',
+            ('portals', 'latlng', 4326, 'POINT', 'XY')
+        )
+        data = [
+            (
+                '1e4669c234ad492e9dfa7b0a2da05cde.16',
+                'Google Volleyball Sand Court', 0, 0, -122.085126, 37.421963
+            ),
+            (
+                '09d5d1e149014c70ba3154fe3421e2a6.12', 'Tetrahelix', 0, 0,
+                -122.089649, 37.423521
+            ),
+            (
+                '9f2eaaa0c1ae4204a2ba5edd46ad4c95.12', "Cupid's Span", 0, 0,
+                -122.390014, 37.791541
+            )
+        ]
+        conn.executemany(
+            'INSERT INTO portals VALUES(?,?,?,?,MakePoint(?,?,4326))', data
+        )
+        conn.commit()
+        expected = frozenset(str(item[5]) for item in data)
+
+        stmt = database.sqlalchemy.select(database.PortalV2)
+        actual = frozenset(
+            row.PortalV2.lat for row in dbc.session.execute(stmt)
+        )
+
+        self.assertEqual(actual, expected)
 
 
 if __name__ == '__main__':  # pragma: no cover
